810221ed7d023b6b9d3cac67046e50c69728c902
Aleh <oleh@tut.by>
2022-02-11 12:18:18 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -36,9 +36,29 @@ const MORSE_TABLE = {
     '----.':  '9',
     '-----':  '0',
 };
-
+const MORSE_CODES = {
+  '10': '.',
+  '11': '-',
+  '**': ' ',
+}
 function decode(expr) {
-    // write your solution here
+  let min = [];
+  for (let i = 0; i < expr.length; i += 10) {
+    min.push(expr.slice(i, i + 10).replace(/^0+/, ''))
+  }
+
+  let world = min.map(element => {
+    let value = [];
+    for (let i = 0; i < element.length; i += 2) {
+      value.push(element.slice(i, i + 2))
+    } return value
+  })
+    .map(element => element
+    .map(value => MORSE_CODES[value]))
+    .map(element => element.join(''))
+    .map(element => MORSE_TABLE[element]).join('')
+
+  return world
 }
 
 module.exports = {
