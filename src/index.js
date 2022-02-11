3638bf4977d49fb787232acd24631b85c74b3e68
Aleh <oleh@tut.by>
2022-02-11 12:20:27 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -44,22 +44,23 @@ const MORSE_CODES = {
 function decode(expr) {
   let min = [];
   for (let i = 0; i < expr.length; i += 10) {
-    min.push(expr.slice(i, i + 10).replace(/^0+/, ''))
+    min.push(expr.slice(i, i + 10).replace(/^0+/, ''));
   }
 
-  let world = min.map(element => {
+  let world =[];
+  world = min.map(element => {
     let value = [];
     for (let i = 0; i < element.length; i += 2) {
       value.push(element.slice(i, i + 2))
-    } return value
+    } return value;
   })
     .map(element => element
     .map(value => MORSE_CODES[value]))
     .map(element => element.join(''))
     .map(element => MORSE_TABLE[element]).join('')
 
-  return world
-}
+  return world;
+};
 
 module.exports = {
     decode
